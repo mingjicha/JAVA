9d5bcbc231d1ba33ad984cb4a075cb26c196c1c0
mingjicha <mingi9100@naver.com>
2024-03-16 22:59:09 +0900

    연산자 초급 정리

--- src/variable/Var6.java
+++ src/variable/Var6.java
@@ -3,6 +3,6 @@ package variable;
 public class Var6 {
     public static void main(String[] args) {
         int a;
-        System.out.println(a);
+        // System.out.println(a); // 에러가 나면 주석 처리
     }
 }
